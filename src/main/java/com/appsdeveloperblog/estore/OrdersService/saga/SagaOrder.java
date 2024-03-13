1f257f84df728ef38ee1869f6698b0d2d7908b0a
ShrawanDadarwa <shrawan.dadarwal@neosoftmail.com>
2024-03-13 21:25:51 +0530

    development

---
+++ src/main/java/com/appsdeveloperblog/estore/OrdersService/saga/SagaOrder.java
@@ -0,0 +1,48 @@
+package com.appsdeveloperblog.estore.OrdersService.saga;
+
+import com.appsdeveloperblog.estore.OrdersService.command.commands.ReserveProductCommand;
+import com.appsdeveloperblog.estore.OrdersService.core.events.OrderCreatedEvent;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.CommandCallback;
+import org.axonframework.commandhandling.CommandMessage;
+import org.axonframework.commandhandling.CommandResultMessage;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.modelling.saga.EndSaga;
+import org.axonframework.modelling.saga.SagaEventHandler;
+import org.axonframework.modelling.saga.StartSaga;
+import org.axonframework.spring.stereotype.Saga;
+
+import javax.annotation.Nonnull;
+
+@Saga
+@RequiredArgsConstructor
+public class SagaOrder {
+
+    private final transient CommandGateway commandGateway;
+    @StartSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void handle(OrderCreatedEvent orderCreatedEvent){
+        ReserveProductCommand reserveProductCommand =  ReserveProductCommand.builder()
+                .productId(orderCreatedEvent.getProductId())
+                .orderId(orderCreatedEvent.getOrderId())
+                .quantity(orderCreatedEvent.getQuantity())
+                .userId(orderCreatedEvent.getUserId()).build();
+
+        commandGateway.send(reserveProductCommand, new CommandCallback<ReserveProductCommand, Object>() {
+
+            @Override
+            public void onResult(@Nonnull CommandMessage<? extends ReserveProductCommand> commandMessage, @Nonnull CommandResultMessage<?> commandResultMessage) {
+                if(commandResultMessage.isExceptional()){
+                    //Start a compensating transaction
+                }
+            }
+        });
+    }
+
+
+    @EndSaga
+    @SagaEventHandler(associationProperty = "orderId")
+    public void endHandler(){
+
+    }
+}
